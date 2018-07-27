e6f77b37829285b75c24d4dd43f9fafc60b0a0da
Isabelle-Grimesey <41082201+Isabelle-Grimesey@users.noreply.github.com>
2018-07-27 15:26:14 -0500

    Add files via upload

--- publish_review_button.js
+++ publish_review_button.js
@@ -1,10 +1,24 @@
-function make_json(form){
-  var json = {
-    ("title":form.first.value),
-    ("organization":form.second.value),
-    ("location":form.third.value),
-    ("rating":form.fourth.value),
-    ("description":form.fifth.value)
-  };
+function WriteToFile() {
 
+  title=document.getElementById("title").value;
+  organization=document.getElementById("organization").value;
+   // alert("title: " +title)
+  location=document.getElementById("location").value;
+  rating=document.getElementById("rating").value;
+  description=document.getElementById("description").value;
+ var reviewSingle = {
+    "title":title,
+    "organization":organization,
+    "location":location,
+    "rating":rating,
+    "description":description
+  };
+// alert("variable successfully created!")
+// reviewSingle = document.getElementById('display').innerHTML;
+
+// //open the file containing all past results and append them to our current list
+// f = open("all_answers.json", "r")
+// olddata = json.load(f)
+// listAllUsers.extend(olddata)
+// f.close()
 }
